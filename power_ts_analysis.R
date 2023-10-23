b42dda7431ccdb5c43a8a4d913b4012f165a5b6c
Ian Hultman <Hultman.Ian@gmail.com>
2023-10-22 19:23:50 -0500

    Initialized repo.

---
+++ power_ts_analysis.R
@@ -0,0 +1,11 @@
+library(R.matlab)
+
+data_dir = paste0(
+  "/home",
+  "/ikhultman",
+  "/Desktop",
+  "/power_time_series_analysis",
+  "/data/");
+
+data_files = paste0(data_dir, list.files(data_dir) );
+
