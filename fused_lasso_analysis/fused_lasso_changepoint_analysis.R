4c884a252d7d4a53726354d3a2875afadc4f10f7
Ian Hultman <Hultman.Ian@gmail.com>
2024-01-26 09:38:26 -0600

    Finished changepoint script. Still need to parallelize.

--- fused_lasso_analysis/fused_lasso_changepoint_analysis.R
+++ fused_lasso_analysis/fused_lasso_changepoint_analysis.R
@@ -1,5 +1,5 @@
 library(genlasso)
-#library(ggplot2)
+library(ggplot2)
 library(purrr)
 library(stringr)
 
@@ -35,6 +35,43 @@ if (!dir.exists(save_dir) ) {
   stopifnot(dir.exists(save_dir) );
 }
 
+# Location where the plots constructed by this fused lasso analysis will be saved.
+figures_dir = paste0(save_dir, "figures/");
+
+if (!dir.exists(figures_dir) ) {
+  dir.create(figures_dir);
+
+  stopifnot(dir.exists(figures_dir) );
+}
+
+# Location where the plots showing how mean test MSE changes with increasing
+# lambda values will be saved. These plots also show a one standard error region
+# around the smallest mean test MSE which is used to find the largest lambda
+# value that provides decent prediction results.
+figures_lam_choice_dir = paste0(figures_dir, "lambda_choice/");
+
+if (!dir.exists(figures_lam_choice_dir) ) {
+  dir.create(figures_lam_choice_dir);
+
+  stopifnot(dir.exists(figures_lam_choice_dir) );
+}
+
+# Location where the plots comparing the observed data to the fused lasso
+# fitted values for each brain region will be saved.
+figures_fit_v_obs_dir = paste0(figures_dir, "fitted_vs_observed/");
+
+if (!dir.exists(figures_fit_v_obs_dir) ) {
+  dir.create(figures_fit_v_obs_dir);
+
+  stopifnot(dir.exists(figures_fit_v_obs_dir) );
+}
+
+dir.create(paste0(figures_fit_v_obs_dir, "lambda_1se") );
+stopifnot(dir.exists(paste0(figures_fit_v_obs_dir, "lambda_1se") ));
+
+dir.create(paste0(figures_fit_v_obs_dir, "lambda_min") );
+stopifnot(dir.exists(paste0(figures_fit_v_obs_dir, "lambda_min") ));
+
 mean_logpower_ts_data = read.csv(mean_logpower_ts_filename, header=TRUE);
 
 expected_nonpow_colnames = c("mouse_id", "freq_band", "region");
@@ -55,37 +92,20 @@ n_power_pts = length(power_col_ixs);
 # Construct the D penalty matrix for fused lasso regression.
 D = getDtf(n_power_pts, 0);
 
+# Plots background color.
+bg_color = rgb(245, 245, 245, maxColorValue=255);
+
+n_reg_freq_combos = nrow(unique(mean_logpower_ts_data[,c("region", "freq_band")]) );
+
+# The fitted values at each frequency for each pair of brain regions will
+# be stored in the following table and saved to a CSV file.
+results_fitted_1se = as.data.frame(matrix(NA, n_power_pts, n_reg_freq_combos) );
+results_fitted_min = as.data.frame(matrix(NA, n_power_pts, n_reg_freq_combos) );
+
 brain_regions = unique(mean_logpower_ts_data$region);
 n_regions = length(brain_regions);
 
-for (rx in 1:n_regions) {
-  next_region = brain_regions[rx];
-  next_region_ixs = mean_logpower_ts_data$region == next_region;
-  next_region_data = mean_logpower_ts_data[next_region_ixs,];
-  rownames(next_region_data) = NULL;
-
-  freq_bands = unique(next_region_data$freq_band);
-  n_freq_bands = length(freq_bands);
-
-  for (fx in 1:n_freq_bands) {
-    next_freq_band = freq_bands[fx];
-    next_freq_ixs = next_region_data$freq_band == next_freq_band;
-    next_reg_freq_data = next_region_data[next_freq_ixs,];
-    rownames(next_reg_freq_data) = NULL;
-
-    n_mice = length(unique(next_reg_freq_data$mouse_id) );
-
-    stopifnot(nrow(next_reg_freq_data) == n_mice);
-
-    # Construct the X design matrix for fused lasso regression.
-    X = diag(n_power_pts);
-    X = t(matrix(rep(X, n_mice), n_power_pts) );
-
-    y_scaled = as.vector(scale(t(as.matrix(next_reg_freq_data[,power_col_ixs]) )));
-    #y = as.vector(t(as.matrix(next_reg_freq_data[,power_col_ixs]) ));
-  }
-}
-
+col_ix = 0;
 
 fused_lasso_loocv = function(Y, X, D, log_lambdas=NA, gamma=0) {
 # DESCRIPTION:
@@ -130,7 +150,7 @@ fused_lasso_loocv = function(Y, X, D, log_lambdas=NA, gamma=0) {
 
   if (any(is.na(log_lambdas) )) {
     n_lambdas = 100;
-    log_lam_min = 1e-6;
+    log_lam_min = log(1e-6);
     log_lam_max = log(
       ceiling(
         max(
@@ -171,288 +191,231 @@ fused_lasso_loocv = function(Y, X, D, log_lambdas=NA, gamma=0) {
   loocv_results
 };
 
+for (rx in 1:n_regions) {
+  next_region = brain_regions[rx];
+  next_region_ixs = mean_logpower_ts_data$region == next_region;
+  next_region_data = mean_logpower_ts_data[next_region_ixs,];
+  rownames(next_region_data) = NULL;
 
-# Fused lasso parameters
-gamma = 0;
-lambdas_1se = rep(NA, n_reg_pairs);
-lambdas_min = rep(NA, n_reg_pairs);
+  freq_bands = unique(next_region_data$freq_band);
+  n_freq_bands = length(freq_bands);
 
-# Location where the plots constructed by this fused lasso analysis will be saved.
-figures_dir = paste0(save_dir, "figures/");
+  for (fx in 1:n_freq_bands) {
+    col_ix = col_ix + 1;
 
-if (!dir.exists(figures_dir) ) {
-  dir.create(figures_dir);
+    next_freq_band = freq_bands[fx];
+    next_freq_ixs = next_region_data$freq_band == next_freq_band;
+    next_reg_freq_data = next_region_data[next_freq_ixs,];
+    rownames(next_reg_freq_data) = NULL;
 
-  stopifnot(dir.exists(figures_dir) );
+    n_mice = length(unique(next_reg_freq_data$mouse_id) );
+
+    stopifnot(nrow(next_reg_freq_data) == n_mice);
+
+    # Construct the X design matrix for fused lasso regression.
+    X = diag(n_power_pts);
+    X = t(matrix(rep(X, n_mice), n_power_pts) );
+
+    y_scaled_mat = scale(t(as.matrix(next_reg_freq_data[,power_col_ixs]) ));
+    Y = as.vector(y_scaled_mat);
+
+    loocv_results = fused_lasso_loocv(Y, X, D);
+    
+    min_tst_mse = min(loocv_results$mean_mse);
+    min_mse_ix = max(which(loocv_results$mean_mse == min_tst_mse) );
+    tst_mse_1se = min_tst_mse + ((loocv_results$sd_mse[min_mse_ix] / sqrt(n_mice) ) * c(-1, 1) );
+    lam_1se_ix = max(which(loocv_results$mean_mse <= tst_mse_1se[2]) );
+    log_lam_1se = loocv_results$log_lambda[lam_1se_ix];
+    log_lam_min = loocv_results$log_lambda[min_mse_ix];
+
+    plot_df1 = data.frame(
+      log_lambda=loocv_results$log_lambda,
+      mean_mse=loocv_results$mean_mse,
+      min_tst_mse=rep(min_tst_mse, nrow(loocv_results) ),
+      mse_1se_lower=rep(tst_mse_1se[1], nrow(loocv_results) ),
+      mse_1se_upper=rep(tst_mse_1se[2], nrow(loocv_results) ));
+
+    rm_ixs = is.infinite(plot_df1$log_lambda) | is.na(plot_df1$log_lambda);
+    plot_df1 = plot_df1[!rm_ixs,];
+
+    plot_df2 = data.frame(
+      log_lambda_min=loocv_results$log_lambda[min_mse_ix],
+      log_lambda_1se=loocv_results$log_lambda[lam_1se_ix]);
+
+    next_mse_results_plot = (
+      ggplot() +
+      theme(
+        panel.background=element_rect(fill=bg_color) ) +
+      geom_line(
+        data=plot_df1,
+        aes(
+          x=log_lambda,
+          y=mean_mse,
+          colour="mean test MSE"),
+        linewidth=1) +
+      geom_vline(
+        data=plot_df2,
+        aes(
+          xintercept=log_lambda_min,
+          colour="log(lambda_min)"),
+        linetype="dotted",
+        linewidth=0.7,
+        show.legend=FALSE) +
+      geom_vline(
+        data=plot_df2,
+        aes(
+          xintercept=log_lambda_1se,
+          colour="log(lambda_1se)"),
+        linetype="dashed",
+        linewidth=0.7,
+        show.legend=FALSE) +
+      geom_ribbon(
+        data=plot_df1,
+        aes(
+          x=log_lambda,
+          y=min_tst_mse,
+          ymin=mse_1se_lower,
+          ymax=mse_1se_upper,
+          colour="1 std. error"),
+        alpha=0.2,
+        show.legend=FALSE) +
+      scale_colour_manual(
+        values=c("gray", "red", "blue", "black"),
+        guide=guide_legend(
+          title="Legend",
+          override.aes=list(
+            fill=c("gray40", bg_color, bg_color, bg_color),
+            linetype=c("solid", "dashed", "dotted", "solid"),
+            linewidth=c(6, 0.7, 0.7, 1) ))) +
+      labs(
+        x="log(lambda)",
+        y="mean test MSE") );
+
+    save_filename = paste0(
+      figures_lam_choice_dir,
+      sprintf(
+        "lambda_choice_power_%s_freq_band_%d.png",
+        next_region,
+        next_freq_band) );
+
+    ggsave(
+      save_filename,
+      next_mse_results_plot,
+      units="in",
+      height=7,
+      width=7,
+      device="png");
+
+    mod_full_data = fusedlasso(Y, X, D, gamma=0);
+
+    results_fitted_1se[,col_ix] = as.numeric(coef(mod_full_data, lambda=exp(log_lam_1se) )$beta);
+    colnames(results_fitted_1se)[col_ix] = paste0(next_region, "_freq_band_", next_freq_band);
+
+    results_fitted_min[,col_ix] = as.numeric(coef(mod_full_data, lambda=exp(log_lam_min) )$beta);
+    colnames(results_fitted_min)[col_ix] = paste0(next_region, "_freq_band_", next_freq_band);
+
+    save_filename = sprintf(
+      "fused_lasso_lambda_1se_power_%s_freq_band_%d.png",
+      next_region,
+      next_freq_band);
+
+    save_filename = paste0(
+      figures_fit_v_obs_dir,
+      "lambda_1se/",
+      save_filename);
+
+    cat(
+      sprintf(
+        paste0(
+          "Saving plot comparing fitted and observed values for ",
+          "lambda w/ 1 std. err. test MSE to:\n%s\n\n"),
+        save_filename) );
+
+    png(filename=save_filename);
+
+    plot(
+      as.numeric(rowMeans(y_scaled_mat) ),
+      lty=2,
+      col="black",
+      main=paste0(
+        next_region,
+        " - frequency band ",
+        next_freq_band),
+      ylab="Scaled Log-power",
+      xlab="Time",
+      type="l",
+      ylim=c(-1, 1) );
+
+    lines(results_fitted_1se[[col_ix]], col="steelblue", lwd=3);
+    legend(
+      34, 3,
+      legend=c("Mean Observed Data", "Fitted Values"),
+      col=c("black", "steelblue"),
+      lty=c(2, 1),
+      lwd=c(1, 3) );
+
+    dev.off();
+
+    save_filename = sprintf(
+      "fused_lasso_lambda_min_power_%s_freq_band_%d.png",
+      next_region,
+      next_freq_band);
+
+    save_filename = paste0(
+      figures_fit_v_obs_dir,
+      "lambda_min/",
+      save_filename);
+
+    cat(
+      sprintf(
+        paste0(
+          "Saving plot comparing fitted and observed values for ",
+          "lambda w/ min test MSE to:\n%s\n\n"),
+        save_filename) );
+
+    png(filename=save_filename);
+
+    plot(
+      as.numeric(rowMeans(y_scaled_mat) ),
+      lty=2,
+      col="black",
+      main=paste0(
+        next_region,
+        " - frequency band ",
+        next_freq_band),
+      ylab="Scaled Log-power",
+      xlab="Time",
+      type="l",
+      ylim=c(-1, 1) );
+
+    lines(results_fitted_min[[col_ix]], col="steelblue", lwd=3);
+    legend(
+      34, 3,
+      legend=c("Mean Observed Data", "Fitted Values"),
+      col=c("black", "steelblue"),
+      lty=c(2, 1),
+      lwd=c(1, 3) );
+
+    dev.off();
+  }
 }
 
-# Location where the plots showing how mean test MSE changes with increasing
-# lambda values will be saved. These plots also show a one standard error region
-# around the smallest mean test MSE which is used to find the largest lambda
-# value that provides decent prediction results.
-figures_lam_choice_dir = paste0(figures_dir, "lambda_choice/");
-
-if (!dir.exists(figures_lam_choice_dir) ) {
-  dir.create(figures_lam_choice_dir);
-
-  stopifnot(dir.exists(figures_lam_choice_dir) );
-}
-
-bg_color = rgb(245, 245, 245, maxColorValue=255);
-
-# Using the leave-one-out cross validation function defined above, determine the
-# lambda parameter value whose corresponding mean test MSE is the minimum observed
-# and the largest lambda parameter whose corresponding mean test MSE falls within
-# one standard error of the smallest mean test MSE for each pair of brain regions.
-for (yx in 1:n_reg_pairs) {
-  cat(
-    paste0(
-      "\nPerforming cross-validation for frequency band selection between ",
-      sprintf("brain regions %s and %s ...\n", region_pairs[yx,1], region_pairs[yx,2]) ));
-
-  results_loocv = fused_lasso_loocv(Y[,yx], X, D, gamma=gamma);
-  min_tst_mse = min(results_loocv$mean_mse);
-  min_mse_ix = max(which(results_loocv$mean_mse == min_tst_mse) );
-  tst_mse_1se = min_tst_mse + ((results_loocv$sd_mse[min_mse_ix] / sqrt(n_mice) ) * c(-1, 1) );
-  lam_1se_ix = max(which(results_loocv$mean_mse <= tst_mse_1se[2]) );
-  lambdas_1se[yx] = results_loocv$lambda[lam_1se_ix];
-  lambdas_min[yx] = results_loocv$lambda[min_mse_ix];
-
-  plot_df1 = data.frame(
-    log_lambda=log(results_loocv$lambda),
-    mean_mse=results_loocv$mean_mse,
-    min_tst_mse=rep(min_tst_mse, nrow(results_loocv) ),
-    mse_1se_lower=rep(tst_mse_1se[1], nrow(results_loocv) ),
-    mse_1se_upper=rep(tst_mse_1se[2], nrow(results_loocv) ));
-
-  rm_ixs = is.infinite(plot_df1$log_lambda) | is.na(plot_df1$log_lambda);
-  plot_df1 = plot_df1[!rm_ixs,];
-
-  plot_df2 = data.frame(
-    log_lambda_min=log(results_loocv$lambda[min_mse_ix]),
-    log_lambda_1se=log(results_loocv$lambda[lam_1se_ix]) );
-
-  next_mse_results_plot = (
-    ggplot() +
-    theme(
-      panel.background=element_rect(fill=bg_color) ) +
-    geom_line(
-      data=plot_df1,
-      aes(
-        x=log_lambda,
-        y=mean_mse,
-        colour="mean test MSE"),
-      linewidth=1) +
-    geom_vline(
-      data=plot_df2,
-      aes(
-        xintercept=log_lambda_min,
-        colour="log(lambda_min)"),
-      linetype="dotted",
-      linewidth=0.7,
-      show.legend=FALSE) +
-    geom_vline(
-      data=plot_df2,
-      aes(
-        xintercept=log_lambda_1se,
-        colour="log(lambda_1se)"),
-      linetype="dashed",
-      linewidth=0.7,
-      show.legend=FALSE) +
-    geom_ribbon(
-      data=plot_df1,
-      aes(
-        x=log_lambda,
-        y=min_tst_mse,
-        ymin=mse_1se_lower,
-        ymax=mse_1se_upper,
-        colour="1 std. error"),
-      alpha=0.2,
-      show.legend=FALSE) +
-    scale_colour_manual(
-      values=c("gray", "red", "blue", "black"),
-      guide=guide_legend(
-        title="Legend",
-        override.aes=list(
-          fill=c("gray40", bg_color, bg_color, bg_color),
-          linetype=c("solid", "dashed", "dotted", "solid"),
-          linewidth=c(6, 0.7, 0.7, 1) ))) +
-    labs(
-      x="log(lambda)",
-      y="mean test MSE") );
-
-  save_filename = paste0(
-    figures_lam_choice_dir,
-    sprintf(
-      "lambda_choice_%s_%s.png",
-      region_pairs[yx,1],
-      region_pairs[yx,2]) );
-
-  ggsave(
-    save_filename,
-    next_mse_results_plot,
-    units="in",
-    height=7,
-    width=7,
-    device="png");
-}
-
-# Location where the plots comparing the observed data to the fused lasso
-# fitted values for each brain region will be saved.
-figures_fit_v_obs_dir = paste0(figures_dir, "fitted_vs_observed/");
-
-if (!dir.exists(figures_fit_v_obs_dir) ) {
-  dir.create(figures_fit_v_obs_dir);
-
-  stopifnot(dir.exists(figures_fit_v_obs_dir) );
-}
-
-dir.create(paste0(figures_fit_v_obs_dir, "lambda_1se") );
-stopifnot(dir.exists(paste0(figures_fit_v_obs_dir, "lambda_1se") ));
-
-dir.create(paste0(figures_fit_v_obs_dir, "lambda_min") );
-stopifnot(dir.exists(paste0(figures_fit_v_obs_dir, "lambda_min") ));
-
-# The fitted values at each frequency for each pair of brain regions will
-# be stored in the following table and saved to a CSV file.
-results_freq_bands_1se = as.data.frame(matrix(NA, n_freq, n_reg_pairs) );
-results_freq_bands_min = as.data.frame(matrix(NA, n_freq, n_reg_pairs) );
-
-for (yx in 1:n_reg_pairs) {
-  cat(
-    sprintf(
-      "Brain regions: %s vs. %s\n",
-      region_pairs[yx,1],
-      region_pairs[yx,2]) );
-
-  cat(paste0("Gamma: ", gamma, "\n") );
-  cat(paste0("Lambda 1 std. err.: ", lambdas_1se[yx], "\n") );
-  cat(paste0("Lambda min.: ", lambdas_min[yx], "\n") );
-
-  # Fit fused lasso model
-  mod = fusedlasso(Y[,yx], X, D, gamma=gamma);
-
-  results_freq_bands_1se[,yx] = as.numeric(coef(mod, lambda=lambdas_1se[yx])$beta);
-  colnames(results_freq_bands_1se)[yx] = paste0(region_pairs[yx,], collapse="-vs-");
-
-  results_freq_bands_min[,yx] = as.numeric(coef(mod, lambda=lambdas_min[yx])$beta);
-  colnames(results_freq_bands_min)[yx] = paste0(region_pairs[yx,], collapse="-vs-");
-
-  save_filename = sprintf(
-    "fused_lasso_lambda_1se_%s_%s.png",
-    region_pairs[yx,1],
-    region_pairs[yx,2]);
-
-  save_filename = paste0(
-    figures_fit_v_obs_dir,
-    "lambda_1se/",
-    save_filename);
-
-  cat(
-    sprintf(
-      paste0(
-        "Saving plot comparing fitted and observed values for ",
-        "lambda w/ 1 std. err. test MSE to:\n%s\n\n"),
-      save_filename) );
-
-  png(filename=save_filename);
-
-  matplot(
-    1:50,
-    matrix(Y[,yx], ncol=n_mice),
-    lty=2,
-    col="black",
-    main=paste(
-      region_pairs[yx,1],
-      "->",
-      region_pairs[yx,2],
-      sep=" "),
-    ylab="Scaled Phase Offset",
-    xlab="Frequency (Hz)",
-    type="l",
-    ylim=c(-3, 3) );
-
-  lines(results_freq_bands_1se[[yx]], col="steelblue", lwd=3);
-  legend(
-    34, 3,
-    legend=c("Observed Data", "Fitted Values"),
-    col=c("black", "steelblue"),
-    lty=c(2, 1),
-    lwd=c(1, 3) );
-
-  dev.off();
-
-  save_filename = sprintf(
-    "fused_lasso_lambda_min_%s_%s.png",
-    region_pairs[yx,1],
-    region_pairs[yx,2]);
-
-  save_filename = paste0(
-    figures_fit_v_obs_dir,
-    "lambda_min/",
-    save_filename);
-
-  cat(
-    sprintf(
-      paste0(
-        "Saving plot comparing fitted and observed values for ",
-        "lambda w/ min test MSE to:\n%s\n\n"),
-      save_filename) );
-
-  png(filename=save_filename);
-
-  matplot(
-    1:50,
-    matrix(Y[,yx], ncol=n_mice),
-    lty=2,
-    col="black",
-    main=paste(
-      region_pairs[yx,1],
-      "->",
-      region_pairs[yx,2],
-      sep=" "),
-    ylab="Scaled Phase Offset",
-    xlab="Frequency (Hz)",
-    type="l",
-    ylim=c(-3, 3) );
-
-  lines(results_freq_bands_min[[yx]], col="steelblue", lwd=3);
-  legend(
-    34, 3,
-    legend=c("Observed Data", "Fitted Values"),
-    col=c("black", "steelblue"),
-    lty=c(2, 1),
-    lwd=c(1, 3) );
-
-  dev.off();
-}
-
-results_freq_bands_1se = cbind(frequency=1:n_freq, results_freq_bands_1se);
-results_freq_bands_min = cbind(frequency=1:n_freq, results_freq_bands_min);
-
-# File where the fused lasso fitted values at each frequency for each pair of
-# brain regions corresponding to the largest lambda parameter value whose
-# mean test MSE falls within one standard error of the minimum will be saved.
 save_filename = paste0(
   save_dir,
-  "selected_frequency_bands_lambda_1se.csv");
+  "fitted_values_lambda_1se.csv");
 
 write.table(
-  results_freq_bands_1se,
+  results_fitted_1se,
   save_filename,
   sep=", ",
   row.names=FALSE);
 
-# File where the fused lasso fitted values at each frequency for each pair of
-# brain regions corresponding to the lambda parameter value with the smallest
-# mean test MSE will be saved.
-
 save_filename = paste0(
   save_dir,
-  "selected_frequency_bands_lambda_min.csv");
+  "fitted_values_lambda_min.csv");
 
 write.table(
-  results_freq_bands_min,
+  results_fitted_min,
   save_filename,
   sep=", ",
   row.names=FALSE);
